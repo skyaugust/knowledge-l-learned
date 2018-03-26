92a6e435e11534fb6b4346feb489e28ba4f3e3ae
wanqiangxin <wanqiangxin@sinovoice.com.cn>
2018-03-26 15:00:18 +0800

    使用countdownlatch控制死锁条件

--- DeadLock.java
+++ DeadLock.java
@@ -1,17 +1,23 @@
+import java.util.concurrent.CountDownLatch;
 public class DeadLock{
     private static Object lockA = new Object();
     private static Object lockB = new Object();
+    private static Boolean condition = false;
+    private static CountDownLatch waitThreadA_hold_lockB = new CountDownLatch(1);
+    private static CountDownLatch waitThreadB_hold_lockA = new CountDownLatch(1);
 
     static class TaskA implements Runnable {
         public void run(){
             synchronized(lockB){
 
                 System.out.println("TaskA hold lockB and ready requires lockA");
+                waitThreadA_hold_lockB.countDown();
                 try {
-                    Thread.sleep(4000); 
+                    waitThreadB_hold_lockA.await();
                 } catch (Exception e) {
                     //TODO: handle exception
                 }
+
                 synchronized(lockA){
                     System.out.println("TaskA hold lockB and lockA");
                 }
@@ -21,9 +27,11 @@ public class DeadLock{
 
     static class TaskB implements Runnable {
         public void run(){
+     
             synchronized(lockA){
                 System.out.println("TaskB hold lockA and ready requires lockB");  
                 System.out.println("TaskB' thread's state:"+Thread.currentThread().getState()); 
+                waitThreadB_hold_lockA.countDown();
                 synchronized(lockB){
                     System.out.println("TaskA hold lockA and lockB");
                 }
@@ -37,7 +45,7 @@ public class DeadLock{
 
         threadA.start();
         try {
-            Thread.sleep(2000); 
+            waitThreadA_hold_lockB.await();
         } catch (Exception e) {
             //TODO: handle exception
         }
